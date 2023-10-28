7e3166f0066a171066207d6d8fa32969a0906fcd
gitdevjin <webdev.hjshin@gmail.com>
2023-10-28 01:59:57 -0400

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import useCart from "../hooks/useCart";
+
+type PropsType = {
+  viewCart: boolean;
+};
+
+const Footer = ({ viewCart }: PropsType) => {
+  const { totalItems, totalPrice } = useCart();
+
+  const year = new Date().getFullYear();
+
+  const pageContent = viewCart ? (
+    <p>Shopping Cart {year}</p>
+  ) : (
+    <>
+      <p>Total items: {totalItems}</p>
+      <p>Total Price: {totalPrice}</p>
+      <p>Shopping Cart: {year}</p>
+    </>
+  );
+  const content = <footer className="footer">{pageContent}</footer>;
+
+  return content;
+};
+
+export default Footer;
